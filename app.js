918b78d30053f713d46757c2a8247a7113b37230
Dorababu Arigi <87510464+Dorababupc@users.noreply.github.com>
2021-12-10 00:10:20 +0530

    final changes

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ var current=document.querySelector("#current-price");
 var quantity=document.querySelector("#no-of-stocks");
 var message=document.querySelector("#display-message");
 function clickHandler(){
+    
     var buy=Number(initial.value);
     var today=Number(current.value);
     var no=Number(quantity.value);
     if(today>buy){
         var gain=today-buy;
         var gainper=(gain/buy)*100;
-        var 
-        message.innerText=
+        var state="Congrats you have gained "+gain +" which is "+gainper+"% of your investment";
+        message.innerText=state;
     }
     else if(today<buy){
         var loss=buy-today;
         var lossper=(loss/buy)*100;
-        message.innerText="Sorry you have lost "+loss+" which is "+lossper+"%";
+        var state="Sorry you have lost "+loss+" which is "+lossper+"% of your investment";
+        message.innerText=state;
     }
     else{
         message.innerText="No gain, no loss";
